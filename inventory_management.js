02fbda25d3aafee5b3a51ed722c4e165600838ec
Rashi Ratan <rashiiratan@gmail.com>
2024-09-14 17:07:51 -0400

    Initialize inventory with product objects

---
+++ inventory_management.js
@@ -0,0 +1,8 @@
+//Task 1: Create an array to store inventory information
+let inventory = [
+    { name: 'Coffee Machine', price: 120, quantity: 14, lowStockLevel: 3 },
+    { name: 'Nespresso', price: 150, quantity: 15, lowStockLevel: 2 },
+    { name: 'Air Fryer', price: 200, quantity: 18, lowStockLevel: 4 },
+    { name: 'Toaster Oven', price: 250, quantity: 24, lowStockLevel: 5 },
+    { name: 'Toaster', price: 100, quantity: 22, lowStockLevel: 7 }
+         ];
